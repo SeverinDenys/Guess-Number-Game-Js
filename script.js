1c0199e8b56016f61b3b66b21792055cc15937e0
SeverinDenys <severindenys367@gmail.com>
2023-12-25 11:30:25 +0100

    adding js functionality

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 'use strict';
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // when there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number😥';
+
+    //when player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉Correct Number!';
+
+    document.querySelector('body').style.background = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    //when guess is to high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        'You have lost the game👏!';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    // when guess is to low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        'You have lost the game👏!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
