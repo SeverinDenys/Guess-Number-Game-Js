0a18824d73c6a3aacceed312e4adaa2f1b936afd
SeverinDenys <severindenys367@gmail.com>
2023-12-26 15:45:24 +0100

    restore page on click and setting the highest score

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 'use strict';
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -13,10 +14,16 @@ document.querySelector('.check').addEventListener('click', function () {
     //when player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.background = '#60b347';
 
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     //when guess is to high
   } else if (guess > secretNumber) {
@@ -42,4 +49,39 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+// Codding challenge #1
+
+/* 
+
+Implement a game rest functionality, so that the player can make a new guess! Here is how:
+
+1.Select the element with the 'again' class and attach a click event handler 
+
+2.In the handler function, restore initial valuers of the score and number variables
+
+3. Restore the initial conditions of the message, number, score and guess input field
+
+number and guess input
+
+4. Also restore the original background color (#222)
+and number width (15rem)
+
+*/
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+
+  document.querySelector('.score').textContent = score;
+
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.background =
+    'linear-gradient(90deg, rgba(2, 0, 36, 1) 0%, rgba(9, 9, 121, 1) 35%, rgba(0, 212, 255, 1) 100%)';
+  document.querySelector('.number').style.width = '15rem';
 });
