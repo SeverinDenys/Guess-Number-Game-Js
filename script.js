8bbc744f2e13ad2f251ea7910e0ec7f1878cac4c
SeverinDenys <severindenys367@gmail.com>
2023-12-26 16:08:23 +0100

    code refactoring

--- script.js
+++ script.js
@@ -4,16 +4,20 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
   // when there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = 'No number😥';
+    displayMessage('No number😥');
 
     //when player wins
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉Correct Number!';
+    displayMessage('🎉Correct Number!');
     document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.background = '#60b347';
@@ -24,34 +28,21 @@ document.querySelector('.check').addEventListener('click', function () {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
     }
-
-    //when guess is to high
-  } else if (guess > secretNumber) {
+    //when guess is wrong
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '📈Too high!';
+      displayMessage(guess > secretNumber ? '📈Too high!' : '📉Too low!');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent =
-        'You have lost the game👏!';
-      document.querySelector('.score').textContent = 0;
-    }
-
-    // when guess is to low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📉Too low!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent =
-        'You have lost the game👏!';
+      displayMessage('You have lost the game👏!');
+      document.querySelector('body').style.background = '#ff0000';
       document.querySelector('.score').textContent = 0;
     }
   }
 });
 
-// Codding challenge #1
+// Codding challenge
 
 /* 
 
@@ -74,7 +65,7 @@ document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-  document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
 
   document.querySelector('.score').textContent = score;
 
